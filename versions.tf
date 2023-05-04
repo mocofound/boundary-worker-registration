3e16d8cbcf12f899917146b2ca247cab9c862669
mocofound <aharness@hashicorp.com>
2023-05-03 22:20:11 -0500

    update from legacy model provider flow

--- versions.tf
+++ versions.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
-    aws = {
-      version = "~> 4.65.0"
-    }
-    boundary = {
-      version = "~> 1.1.5"
-    }
+    # aws = {
+    #   version = "~> 4.65.0"
+    # }
+    # boundary = {
+    #   version = "~> 1.1.5"
+    # }
   }
   required_version = "~> 1.4.6"
 }
