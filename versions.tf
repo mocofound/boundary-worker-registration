b9bbb6606df79e9ffffb4fee4bc0c229f46edac4
mocofound <aharness@hashicorp.com>
2023-05-03 22:21:43 -0500

    update providers.tf

--- versions.tf
+++ versions.tf
@@ -1,12 +1,12 @@
-terraform {
-  # required_providers {
-  #   # aws = {
-  #   #   version = "~> 4.65.0"
-  #   # }
-  #   # boundary = {
-  #   #   version = "~> 1.1.5"
-  #   # }
-  # }
-  # required_version = "~> 1.4.6"
-}
+# terraform {
+#   # required_providers {
+#   #   # aws = {
+#   #   #   version = "~> 4.65.0"
+#   #   # }
+#   #   # boundary = {
+#   #   #   version = "~> 1.1.5"
+#   #   # }
+#   # }
+#   # required_version = "~> 1.4.6"
+# }
 
